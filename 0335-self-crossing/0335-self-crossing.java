f3274a97ce3ef97010ba1228f230f378b63b0632
Jolisha <jolishaangelin@gmail.com>
2026-09-02 15:15:03 +0530

    Time: 2 ms (84.59%), Space: 59.3 MB (66.14%) - LeetHub

---
+++ 0335-self-crossing/0335-self-crossing.java
@@ -0,0 +1,30 @@
+class Solution {
+   public static boolean isSelfCrossing(int[] x) {
+	if (x.length < 4)
+		return false;
+	
+	boolean inside = false;
+	for (int i = 3; i < x.length; i++) {
+		if(inside) {
+			if (x[i] >= x[i - 2])
+				return true;
+			continue;
+		}
+				
+		if(x[i-1] > x[i-3])
+			continue;
+
+		int x5 = i>=5 ? x[i-5] : 0;
+		int x4 = i>=4 ? x[i-4] : 0;
+		if(x[i-1] >= x[i-3] - x5) {
+			if(x[i] >= x[i-2] - x4)
+				return true;
+		}else {
+			if(x[i] >= x[i-2])
+				return true;
+		}
+		inside=true;
+	}
+	return false;
+}
+}
